3d00faaf7f034f570b9717c4a06bbfed19f2a601
clavelclams <clavelclams12@gmail.com>
2023-10-19 09:32:31 +0200

    1.1 Exercice 1 phase 3

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+/* 
 alert ('alerte ceci est une chaine de caractere');
 console.log('ceci est un message');
 var message = prompt('message');
 confirm('confirme moi ');
 var nom =  prompt('saisissez votre nom');
 alert('bonjour ' + nom + ' voici votre message ' + '"' + message + '"' );
+*/
+
+// exercice1.js 
+alert('Coucou !');
+console.log('Ah que Coucou !');
+document.write('Coucou beuh !');
