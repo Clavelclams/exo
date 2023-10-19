76fd6d3f27dd2a391298c48eca41de03741199fb
clavelclams <clavelclams12@gmail.com>
2023-10-19 09:14:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+alert ('alerte ceci est une chaine de caractere');
+console.log('ceci est un message');
+var message = prompt('message');
+confirm('confirme moi ');
+var nom =  prompt('saisissez votre nom');
+alert('bonjour ' + nom + ' voici votre message ' + '"' + message + '"' );
